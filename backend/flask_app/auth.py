51561851cfc4d00ba701f05a5d45fc2932c87dfd
emmaashi <emmashi2006@gmail.com>
2025-01-06 10:51:52 -0500

    update user model

--- backend/flask_app/auth.py
+++ backend/flask_app/auth.py
@@ -51,15 +51,14 @@ def sign_up():
             return jsonify({"message": "User already exists."}), 409
         elif len(email) < 4:
             return jsonify({"message": "Email must be greater than 4 characters."}), 400
-        elif len(first_name) < 2 or len(last_name) < 2:
+        elif len(first_name) < 2:
             return jsonify({"message": "First name must be longer than 1 character."}), 400
         elif password1 != password2:
             return jsonify({"message": "Passwords do not match."}), 400
         elif len(password1) < 7:
             return jsonify({"message": "Password must be at least 7 characters."}), 400
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
-            # new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
+            new_user = User(email=email, first_name=first_name, last_name=last_name, password=generate_password_hash(password1, method='pbkdf2:sha256'))
             db.session.add(new_user)  # Add the new user to the database
             db.session.commit()
             return jsonify({"message": "Account successfully created!", "redirect_url": "/home"}), 201
